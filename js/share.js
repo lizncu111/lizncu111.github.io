10691d33d0dada357ff3d7e839c4746409ffa96c
李欣桐 <JavaCoffee123@noreply.gitcode.com>
2024-12-23 22:19:28 +0800

    Site updated: 2024-12-23 22:19:27

--- js/share.js
+++ js/share.js
@@ -12,8 +12,8 @@ function share_() {
     try {
         // 截取标题
         var title = document.title;
-        var subTitle = title.endsWith("| Fomalhaut🥝") ? title.substring(0, title.length - 14) : title;
-        navigator.clipboard.writeText('Fomalhaut🥝的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
+        var subTitle = title.endsWith("技术と游戏的博客") ? title.substring(0, title.length - 14) : title;
+        navigator.clipboard.writeText('空气力学少女博客的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
         new Vue({
             data: function () {
                 this.$notify({
